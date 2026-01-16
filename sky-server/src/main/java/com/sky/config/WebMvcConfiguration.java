7370dd166b80f7b46a24e20298cfdb33526835ff
FanBingQiang <3581505260@qq.com>
2026-01-16 09:57:32 +0800

    1

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -35,6 +36,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      *
      * @param registry
      */
+    @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
@@ -52,28 +54,14 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * 
      * @return
      */
-    // @Bean
-    // Docket docket() {
-    // log.info("开始生成接口文档...");
-    // ApiInfo apiInfo = new ApiInfoBuilder()
-    // .title("苍穹外卖项目接口文档")
-    // .version("2.0")
-    // .description("苍穹外卖项目接口文档")
-    // .build();
-    // Docket docket = new Docket(DocumentationType.SWAGGER_2)
-    // .apiInfo(apiInfo)
-    // .select()
-    // .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
-    // .paths(PathSelectors.any())
-    // .build();
-    // return docket;
-    // }
+
 
     /**
      * 设置静态资源映射
      * 
      * @param registry
      */
+    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始设置静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -37,6 +37,7 @@ public class EmployeeServiceImpl implements EmployeeService {
      * @param employeeLoginDTO
      * @return
      */
+    @Override
     public Employee login(EmployeeLoginDTO employeeLoginDTO) {
         String username = employeeLoginDTO.getUsername();
         String password = employeeLoginDTO.getPassword();
@@ -58,7 +59,7 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
 
-        if (employee.getStatus() == StatusConstant.DISABLE) {
+        if (employee.getStatus().equals(StatusConstant.DISABLE)) {
             // 账号被锁定
             throw new AccountLockedException(MessageConstant.ACCOUNT_LOCKED);
         }
